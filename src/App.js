a7c07ec7d55db2c64278351e55ba9aa6698c82d6
Julia Schwerdtfeger <js.schwerdt@gmail.com>
2023-12-01 15:52:37 +0100

    solve challenge

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
 import "./styles.css";
 
+function Greeting({ name }) {
+  return <h1>Hello {name} </h1>;
+}
+
 export default function App() {
-  return <h1>Replace me with your component!</h1>;
+  return (
+    <div>
+      <Greeting name={"Julia"} />
+      <Greeting name={"Anna"} />
+    </div>
+  );
 }
